1ca65e4f78154d920db633661a5a762af9b98882
kodaimura <kodaimura123@gmail.com>
2022-05-21 23:40:49 +0900

    first commit

--- chapter2-2.scm
+++ chapter2-2.scm
@@ -266,4 +266,205 @@
 
 (print (square-tree3 t1))
 
+;2.32
+(define (subsets s)
+	(if (null? s)
+		(list '())
+		(let ((rest (subsets (cdr s))))
+			(append rest 
+					(map (lambda (l)
+							(cons (car s) l)) rest)))))
 
+(print (subsets '(1 2 3)))
+
+
+;2.33
+(define (accumulate op initial sequence)
+	(if (null? sequence)
+		initial
+		(op (car sequence)
+			(accumulate op initial (cdr sequence)))))
+
+
+(define (_map p s)
+	(accumulate (lambda (x y)
+					(cons (p x) y)) '() s))
+
+
+(define (_append seq1 seq2)
+	(accumulate cons seq2 seq1))
+
+
+(define (_length sequence)
+	(accumulate (lambda (x y) (+ 1 y)) 0 sequence))
+
+(print (_map square '(1 2 3)))
+(print (map square '(1 2 3)))
+(print (_append '(1 2) '(3 4 5)))
+(print (append '(1 2) '(3 4 5)))
+(print (length '(1 2 3)))
+(print (_length '(1 2 3)))
+
+
+;2.34
+;anX^n + an-1X^n-1 + ... + a1X + a0
+;Hornerの方法
+;((anX + an-1)X + ... + a1)X + a0
+;anから始め、xを掛け、an-1を足し、xを掛け、a0まで繰り返し
+(define (horner-eval x coefficient-sequence)
+	(accumulate (lambda (this-coeff higher-terms)
+					(+ this-coeff (* x higher-terms)))
+				0
+				coefficient-sequence))
+
+;x=2 1 + 3x + 5x^3 + x^5
+(print (horner-eval 2 (list 1 3 0 5 0 1)))
+
+
+;2.35
+(define (count-leaves2 t)
+	(accumulate +
+				0
+				(map (lambda (x) (if (pair? x) (count-leaves x) 1)) t)))
+
+(print (count-leaves2 (list (list 1 2) 3 (list 4) 5)))
+
+
+;2.36
+(define (accumulate-n op init seqs)
+	(if (null? (car seqs))
+		'()
+		(cons (accumulate op init (map car seqs))
+			  (accumulate-n op init (map cdr seqs)))))
+
+(print (accumulate-n + 0 (list (list 1 2 3) (list 4 5 6) (list 7 8 9) (list 10 11 12))))
+
+
+;2.38
+(define fold-right accumulate)
+
+(define (fold-left op init seq)
+	(define (iter result rest)
+		(if (null? rest)
+			result
+			(iter (op result (car rest))
+				  (cdr rest))))
+	(iter init seq))
+
+(print (fold-right / 1 (list 1 2 3)))
+(print (fold-left / 1 (list 1 2 3)))
+(print (fold-right list '() (list 1 2 3)))
+(print (fold-left list '() (list 1 2 3)))
+
+
+;2.39
+(define (reverse1 seq)
+	(fold-right (lambda (x y) (append y (list x)))
+				'()
+				seq))
+
+
+(define (reverse2 seq)
+	(fold-left (lambda (x y) (append (list y) x))
+				'()
+				seq))
+
+(print (reverse1 (list 1 2 3 4 5)))
+(print (reverse2 (list 1 2 3 4 5)))
+
+
+;写像
+(define (enumerate-interval low high)
+	(if (> low high)
+		'()
+		(cons low (enumerate-interval (+ low 1) high))))
+
+(print
+(accumulate append
+			'()
+			(map (lambda (i)
+					(map (lambda (j) (list i j))
+					 	 (enumerate-interval 1 (- i 1))))
+				 (enumerate-interval 1 6)))
+)
+
+(define (flatmap proc seq)
+	(accumulate append '() (map proc seq)))
+
+
+(define (prime-sum? pair)
+	(prime? (+ (car pair) (cadr pair))))
+
+
+(define (make-pair-sum pair)
+	(list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+
+
+(define (prime? x)
+	(define (iter n) 
+		(cond 
+			((< x (square n)) #t)
+			((zero? (remainder x n)) #f)
+			(else
+				(iter (+ n 2)))))
+	(if (or (< x 2) (and (not (= x 2)) (even? x))) #f (iter 3)))
+
+
+(define (prime-sum-pairs n)
+	(map make-pair-sum
+		(filter prime-sum?
+			(flatmap
+				(lambda (i)
+					(map (lambda (j) (list i j))
+						 (enumerate-interval 1 (- i 1))))
+				(enumerate-interval 1 n)))))
+
+(print (prime-sum-pairs 6))
+
+
+;2.40
+(define (unique-pairs n)
+	(accumulate append
+			'()
+			(map (lambda (i)
+		 			(map (lambda (j) (list i j))
+			  	 (enumerate-interval 1 (- i 1))))
+		 	(enumerate-interval 1 n))))
+
+
+(define (prime-sum-pairs2 n)
+	(map make-pair-sum
+		 (filter prime-sum?
+			(unique-pairs n))))
+
+(print (unique-pairs 6))
+(print (prime-sum-pairs2 6))
+
+
+;2.41
+(define (permutations s)
+	(accumulate append 
+			    (list (list)) 
+			    (map (lambda (x) 
+			       		(map (lambda (p) (cons x p))
+			       				(permutations (remove x s))))
+			       	  s)))
+
+(define (remove item seq)
+	(filter (lambda (x) (not (= x item)))
+			seq))
+
+(print (permutations (list 1 2 3 4)))
+
+
+(define (sum-list ls)
+	(if (null? ls)
+		0
+		(+ (car ls) (sum-list (cdr ls)))))
+
+
+(define (c2q41 n s)
+	(filter (lambda (l) (and (= (length l) 3) (= (sum-list l) s)))
+		    (permutations (enumerate-interval 1 n))))
+
+(print (c2q41 5 7))

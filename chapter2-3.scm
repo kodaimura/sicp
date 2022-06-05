e6c6865c6f00018a0d7d179c9bd062e295fd4ba1
kodaimura <kodaimura123@gmail.com>
2022-06-05 23:58:52 +0900

    first commit

--- chapter2-3.scm
+++ chapter2-3.scm
@@ -226,3 +226,117 @@
 
 (print (intersection-set '(1 2 3) '(3 4 5)))
 (print (union-set '(1 2 3) '(3 4 5)))
+
+;順序づけられたリストとしての集合
+(define (element-of-set2? x set)
+	(cond 
+		((null? set) #f)
+		((= x (car set)) #t)
+		((< x (car set)) #f)
+		(else (element-of-set2? x (cdr set)))))
+
+
+;2.61
+(define (adjoin-set2 x set)
+	(cond
+		((null? set) (list x))
+		((= x (car set)) set)
+		((< x (car set)) (cons x set))
+		(else (cons (car set) (adjoin-set2 x (cdr set))))))
+
+(define set0 '(3 7 9))
+(print (adjoin-set2 2 set0))
+(print (adjoin-set2 6 set0))
+(print (adjoin-set2 9 set0))
+
+
+;2.62
+(define (union-set2 set1 set2)
+	(cond
+		((null? set1) set2)
+		((null? set2) set1)
+		(else
+			(let ((x1 (car set1))
+			      (x2 (car set2)))
+				(cond 
+					((= x1 x2) (cons x1 (union-set2 (cdr set1) (cdr set2))))
+					((< x1 x2) (cons x1 (union-set2 (cdr set1) set2)))
+					(else (cons x2 (union-set2 set1 (cdr set2)))))))))
+
+(print (union-set2 '(1 3 6) '(2 4 5)))
+(print (union-set2 '(1 3 6) '(4 5)))
+(print (union-set2 '(1 3 6) '(4 5 6)))
+
+
+;二進木としての集合
+(define (entry tree) (car tree))
+
+(define (left-branch tree) (cadr tree))
+
+(define (right-branch tree) (caddr tree))
+
+(define (make-tree entry left right)
+	(list entry left right))
+
+(define (element-of-set3? x set)
+	(cond
+		((null? set) #f)
+		((= x (entry set)) #t)
+		((< x (entry set))
+			(element-of-set3? x (left-branch set)))
+		((> x (entry set))
+			(element-of-set3? x (right-branch set)))))
+
+(define (adjoin-set3 x set)
+	(cond
+		((null? set) (make-tree x '() '()))
+		((= x (entry set)) set)
+		((< x (entry set))
+			(make-tree (entry set)
+				       (adjoin-set3 x (left-branch set))
+				       (right-branch set)))
+		((> x (entry set))
+			(make-tree (entry set)
+				       (left-branch set)
+				       (adjoin-set3 x (right-branch set))))))
+
+;2.63
+(define (tree->list-1 tree)
+	(if (null? tree)
+		'()
+		(append (tree->list-1 (left-branch tree))
+				(cons (entry tree)
+				  	  (tree->list-1 (right-branch tree))))))
+
+(define (tree->list-2 tree)
+	(define (copy-to-list tree result-list)
+		(if (null? tree)
+			result-list
+			(copy-to-list (left-branch tree)
+				          (cons (entry tree)
+				          	    (copy-to-list (right-branch tree)
+				          	    	          result-list)))))
+	(copy-to-list tree '()))
+
+
+;2.64
+(define (list->tree elements)
+	(car (partial-tree elements (length elements))))
+
+(define (partial-tree elts n)
+	(if (= n 0)
+		(cons '() elts)
+		(let ((left-size (quotient (- n 1) 2)))
+          (let ((left-result (partial-tree elts left-size)))
+            (let ((left-tree (car left-result))
+                  (non-left-elts (cdr left-result))
+                (right-size (- n (+ left-size 1))))
+            	  (let ((this-entry (car non-left-elts))
+                  		(right-result (partial-tree (cdr non-left-elts)
+                               		  right-size)))
+              	    (let ((right-tree (car right-result))
+                          (remaining-elts (cdr right-result)))
+               		  (cons (make-tree this-entry left-tree right-tree)
+                      		remaining-elts))))))))
+
+(print (list->tree '(1 3 5 7 9 11)))
